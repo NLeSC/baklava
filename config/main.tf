b5e0d6079824aabcfda1fb6947f2f4ffe2bc98aa
Faruk D <f.diblen@esciencecenter.nl>
2019-05-09 12:06:13 +0200

    add configuration files and templates

---
+++ config/main.tf
@@ -0,0 +1,58 @@
+provider "opennebula" {
+        endpoint = "${var.endpoint_url}"
+        username = "${var.one_username}"
+        password = "${var.one_password}"
+}
+
+data "template_file" "one-kube-template" {
+        template = "${file("opennebula_k8s.tpl")}"
+}
+
+resource "opennebula_template" "one-kube-template" {
+        name = "k8s-template"
+        description = "${data.template_file.one-kube-template.rendered}"
+        permissions = "600"
+}
+
+resource "opennebula_vm" "kube-node" {
+        name = "k8s-node${count.index}"
+        template_id = "${opennebula_template.one-kube-template.id}"
+        permissions = "600"
+        count = "${var.number_of_nodes}"
+}
+
+resource "null_resource" "kubernetes" {
+
+        provisioner "local-exec" {
+                command = "cp -rfp /baklava/kubespray/inventory/sample /baklava/kubespray/inventory/${var.cluster_name}"
+        }
+
+        provisioner "local-exec" {
+                command = "/bin/bash -c \"declare -a IPS=(${join(" ", opennebula_vm.kube-node.*.ip)})\""
+        }
+
+        provisioner "local-exec" {
+                command = "CONFIG_FILE=/baklava/kubespray/inventory/${var.cluster_name}/hosts.yaml python3 /baklava/kubespray/contrib/inventory_builder/inventory.py ${join(" ", opennebula_vm.kube-node.*.ip)}"
+        }
+
+        provisioner "local-exec" {
+                command = "sleep 30; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i /baklava/kubespray/inventory/${var.cluster_name}/hosts.yaml /baklava/config/firewall.yml --private-key=/baklava/id_rsa_baklava"
+        }
+
+#        provisioner "local-exec" {
+#                command = "cp -fp /baklava/config/kubespray_addons.yml /baklava/kubespray/inventory/${var.cluster_name}/group_vars/k8s-cluster/addons.yml"
+#        }
+
+        provisioner "local-exec" {
+                command = "sleep 30; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i /baklava/kubespray/inventory/${var.cluster_name}/hosts.yaml /baklava/kubespray/cluster.yml --private-key=/baklava/id_rsa_baklava"
+        }
+}
+
+output "kube-node-vm_id" {
+        value = "${opennebula_vm.kube-node.*.id}"
+}
+
+output "kube-node-vm_ip" {
+        value = "${opennebula_vm.kube-node.*.ip}"
+}
+
---
+++ config/variables.tf
@@ -0,0 +1,5 @@
+variable "endpoint_url" { default = "https://api.hpccloud.surfsara.nl/RPC2" }
+variable "one_username" { default = "USERNAME" }
+variable "one_password" { default = "PASSWORD" }
+variable "cluster_name" { default = "k8s_cluster" }
+variable "number_of_nodes" { default = 4 }
